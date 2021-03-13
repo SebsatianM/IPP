41ce7ddcefe34c080a1795a2a032196bfa8942e4
Sebastian Mielczarek <sebastian.mielczarek@op.pl>
2021-03-13 16:55:01 +0100

    pierszy commit poczatek tworzenia gui

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import tkinter as tk
-=======
 from tkinter import *
->>>>>>> fix_center_btn
 from tkinter import ttk
 
 #define colors palette
@@ -12,44 +8,6 @@ c_grey = "#C5C6C7"
 c_light_blue = "#66FCF1"
 c_turquoise = "#45A29E"
 
-<<<<<<< HEAD
-root = tk.Tk()
-root.title('Sorting Algorithms Visualizer')
-root.maxsize(900,600)
-root.minsize(500,400)
-root.config(bg=c_black,pady=5,padx=5)
-
-
-ui_frame = tk.Frame(root, bg=c_dark_grey,width=500,height= 200)
-ui_frame.grid(column=0, row=0, sticky=tk.N+tk.S+tk.E+tk.W)
-
-root.columnconfigure(0, weight=1)
-root.rowconfigure(1, weight=1)
-ui_frame.grid_columnconfigure(0,weight=1)
-
-def show_sorting_options():
-    tk.Label(ui_frame,
-        text="Wybierz algorytm:",
-        foreground=c_black,  
-        background=c_grey,  
-    ).grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
-
-    tk.Label(ui_frame,
-        text="Ustaw prędkość:",
-        foreground=c_black,  
-        background=c_grey,  
-        
-    ).grid(row=2, column=0, padx=5, pady=5, sticky=tk.W)
-
-
-
-
-tk.Button(ui_frame,text="Sortowanie",bg =c_grey,fg=c_black,padx=5,pady=5,command=show_sorting_options).grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
-tk.Button(ui_frame,text="Całkowanie",bg =c_grey,fg=c_black,padx=5,pady=5,command=show_sorting_options).grid(row=0, column=1, padx=5, pady=5)
-tk.Button(ui_frame,text="Miejsca zerowe",bg =c_grey,fg=c_black,padx=5,pady=5,command=show_sorting_options).grid(row=0, column=2, padx=5, pady=5, sticky=tk.E)
-
-root.mainloop()
-=======
 #Mainwindows settings
 root = Tk()
 root.title('Sorting Algorithms Visualizer')
@@ -139,4 +97,3 @@ if __name__ == "__main__":
 
 
     root.mainloop()
->>>>>>> fix_center_btn
